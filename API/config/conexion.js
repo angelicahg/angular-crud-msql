166f0ce593682dee3ca0aaaecbd2904ddd77e0ba
Angelica Herrera <herrerangelik@gmail.com>
2021-09-28 21:27:02 -0500

    backend

--- API/config/conexion.js
+++ API/config/conexion.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
 const conexion = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  port: 3306,
-  database: 'movie'
+
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 
 
 });
--- API/index.js
+++ API/index.js
@@ -1,7 +1,7 @@
 require('./config/conexion');
 
 const express = require('express');
-const port = (process.env.port || 3000);
+const port = (process.env.PORT || 3000);
 
 // express
 const app = express();
